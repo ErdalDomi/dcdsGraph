9c064957c1a7682cb5c085ac33b211de5a48051b
ErdalDomi <erdal.domi2@gmail.com>
2017-04-24 17:04:56 +0200

    added connect to db functionality and libraries

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 var express = require('express')
 var app = express()
 var path = require('path')
+var Client = require('pg').Client;
+var connection = require('pg').Connection;
+var bodyParser = require('body-parser')
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+var client;
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 // app.get('/dbconnect', function(req, res){
 //   res.send('trying to connect to db')
 // })
-
+app.post("/dbconnect", function(request, response){
+  console.log("got a dbconnect psot request");
+  var username = request.body.username;
+  var password = request.body.password;
+  var dbname = request.body.dbname;
+  //console.log("username: " + username + " \npassword: " + password + "\ndbname: " + dbname);
+  client = new Client({
+      user: username, //'postgres', //replace with username
+      password: password, //'password', //password
+      database: dbname,//'travel', //dbname
+      host: '127.0.0.1',
+      port: 5432
+    });
+  connection = client.connect(function(err){
+    if(err){
+      console.log("error connecting ", err);
+    }
+  });
+  console.log('Database connection established');
+  response.redirect('back');
+});
 app.listen(8000,function(){
   console.log('Listening on port 8000')
 })
