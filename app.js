926fc8d48a7f4ce42a2ca75ca115bf9cc751a217
ErdalDomi <erdal.domi2@gmail.com>
2017-04-24 21:50:37 +0200

    added load graph button and functionality

--- app.js
+++ app.js
@@ -6,38 +6,76 @@ var connection = require('pg').Connection;
 var bodyParser = require('body-parser')
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended:true}));  // to support URL-encoded bodies
 
-var client;
 
+
+var client; //we use this to query the database
+
+//this is to serve static files like html and css from
+//the public folder
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.post('/test', function(req, res){
-  console.log(req.body.username + " " +req.body.password);
-})
+/*This will get the information from the ajax request and
+create a connection to the db. Then we use the variable
+client to query the database.*/
 app.post("/dbconnect", function(request, response){
   console.log("got a dbconnect psot request");
   var username = request.body.username;
   var password = request.body.password;
   var dbname = request.body.dbname;
-  //console.log("username: " + username + " \npassword: " + password + "\ndbname: " + dbname);
+  var connectionStatus = "yes"; //to keep track of the connection status
   client = new Client({
-      user: username, //'postgres', //replace with username
-      password: password, //'password', //password
-      database: dbname,//'travel', //dbname
+      user: username,
+      password: password,
+      database: dbname,
       host: '127.0.0.1',
       port: 5432
     });
   connection = client.connect(function(err){
     if(err){
       console.log("error connecting ", err);
+      connectionStatus = "no"; //still an issue when we dont get a proper connection
     }
   });
-  console.log('Database connection established');
-  response.send('hey.');
+  console.log('Connected to '+dbname + ' as user ' +username);
+  response.send(connectionStatus);
 });
-app.listen(8000,function(){
-  console.log('Listening on port 8000')
+
+//Start the app
+var port = 8000;
+app.listen(port,function(){
+  console.log('Listening on port ' + port)
 })
+
+//This will load nodes from the connection table
+//and put them into the resopnse array as an object
+app.get("/loadNodes", function(request, response){
+
+  var query = client.query('select * from connection');
+  var responseArray = [];
+  query.on('row', function(row, result) {
+    curr = {id: row.id, label: row.name};
+    responseArray.push(curr);
+  });
+  setTimeout(function(){
+    console.time('server loadNodes');
+    response.send(responseArray);
+    console.timeEnd('server loadNodes');
+  }, 50 );
+});
+
+app.get("/loadEdges", function(request, response){
+
+  var query = client.query('select * from edges');
+  var responseArray = [];
+  query.on('row', function(row, result){
+    curr = {from: row.frosm, to: row.tso, arrows: 'to'};
+    responseArray.push(curr);
+  });
+  setTimeout(function(){
+    console.time('server loadEdges');
+    response.send(responseArray);
+    console.timeEnd('server loadEdges');
+  }, 50);
+});
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,36 +2,16 @@ $( document ).ready(function() {
     console.log( "ready!" );
     $('.ui.accordion').accordion();
     $('.ui.accordion').accordion({exclusive: false});
-    var nodes = new vis.DataSet([
-        {id: 1, label: 'Node 1'},
-        {id: 2, label: 'Node 2'},
-        {id: 3, label: 'Node 3'},
-        {id: 4, label: 'Node 4'},
-        {id: 5, label: 'Node 5'}
-      ]);
-
-      // create an array with edges
-      var edges = new vis.DataSet([
-        {from: 1, to: 3},
-        {from: 1, to: 2},
-        {from: 2, to: 4},
-        {from: 2, to: 5},
-        {from: 3, to: 3}
-      ]);
-
-      // create a network
-      var container = document.getElementById('mynetwork');
-      var data = {
-        nodes: nodes,
-        edges: edges
-      };
-      var options = {};
-      var network = new vis.Network(container, data, options);
-
 });
 
-
-function myFunction(){
+/* --------------------------------------------------------------------
+This connects to the database given the inputs from
+the form. The variables though are sent with an ajax request.
+The line xhttp.send() sends the information to the server as a parameter.
+I don't know the security implications of this and find a more accurate
+way to send some input from a form securely with ajax and ExpressJS.
+----------------------------------------------------------------------*/
+function connectDB(){
   var username = document.getElementsByName('username')[0].value;
   var password = document.getElementsByName('password')[0].value;
   var dbname = document.getElementsByName('dbname')[0].value;
@@ -40,11 +20,89 @@ function myFunction(){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
-      alert(this.responseText);
+      alert(this.responseText); //this is where we change the connection status. do some jquery
     }
   }
   xhttp.open("post", "/dbconnect", true);
   xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  var object = {username: username};
   xhttp.send('username='+username+'&password='+password+'&dbname='+dbname);
 }
+
+/* This function will load the initial graph on the browser. */
+var nodes = [];
+var edges = [];
+var network;
+var options;
+
+function loadGraph(){
+  console.time('loading network');
+  loadNodes();
+  loadEdges();
+  console.timeEnd('loading network');
+
+  setTimeout(function(){
+    //We need to "treat" the nodes so we convert them to a format vis.js can understand
+    var treatedNodes = [];
+    JSON.parse(nodes).forEach(function(currentNode){
+      var curr = {id: currentNode.id, label: currentNode.id};
+      treatedNodes.push(curr);
+    });
+    console.log(treatedNodes);
+    var treatedEdges = [];
+    JSON.parse(edges).forEach(function(currentEdge){
+      var curr = {from: currentEdge.from, to: currentEdge.to, arrows: currentEdge.arrows};
+      treatedEdges.push(curr);
+    });
+    console.log(treatedEdges);
+    var container = document.getElementById('mynetwork');
+    var data = {
+      nodes: treatedNodes,
+      edges: treatedEdges
+    };
+    options = {};
+    network = new vis.Network(container, data, options);
+  }, 100); // + - ?
+
+}
+
+/*This function will load the edge information from the database.
+The information is saved on the edges variable. The format of the
+information is an array of objects that hold necessary edge information.*/
+function loadEdges(){
+  console.time('loadEdges');
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      edges = this.responseText;
+      console.timeEnd('loadEdges');
+    }
+  }
+  xhttp.open("GET", "/loadEdges", true);
+  xhttp.send();
+}
+
+/*The same principle applies here too*/
+function loadNodes(){
+  console.time('loadNodes');
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      nodes = this.responseText;
+      console.timeEnd('loadNodes');
+    }
+  }
+  xhttp.open("GET", "/loadNodes", true);
+  xhttp.send();
+}
+/* In this part we get the network div and add an event listener to it
+in order to interact with nodes */
+var graphDiv = document.getElementById('mynetwork');
+graphDiv.addEventListener('click', function(event){
+  network.on("click", function (params) {
+    params.event = "[original event]";
+    var nr = params.nodes[0];
+    var x = JSON.parse(nodes);
+    console.log(x[nr-1].label);
+    document.getElementById('nodeName').innerHTML = (""+x[nr-1].label);
+  });
+})
